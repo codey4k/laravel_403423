33fc572eebb388d769db2988defdf161fcf1c14a
codey4k <codey4k@gmail.com>
2018-05-07 21:20:20 +0000

    update style - header component

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,4 @@
-<div class="header">
-    <h1>{{ $title }}</h1>
-</div>
+<header class="header">
+    <img src="{{ asset('images/security.jpeg') }}" alt="header image" class="header__img" />
+    <h1 class="header__title">{{ $title }}</h1>
+</header>
