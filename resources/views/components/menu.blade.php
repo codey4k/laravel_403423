2c79342016a2743900aa14386f4c9428fd5f91cd
codey4k <codey4k@gmail.com>
2018-05-11 22:37:31 +0000

    update styles of menu component

--- resources/views/components/menu.blade.php
+++ resources/views/components/menu.blade.php
@@ -1,5 +1,11 @@
 <div class="menu" id="menu">
     <div class="menu__container">
+        <h1 class="menu__logo">Logo</h1>
         
+        <nav class="menu__nav">
+            <li class="menu__item">Web</li>
+            <li class="menu__item">Contact</li>
+            <li class="menu__item">About us</li>
+        </nav>
     </div>
 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,9 @@
 @section('title', 'Home')
 
 @section('content')
-
+    
+    @include('components/menu')
+    
     @include('components/header', ['title' => 'Welcome'])
     
     @include('components/footer')
