a85a3a8fe74387410d3557f43a803a98afb8036d
codey4k <codey4k@gmail.com>
2018-05-28 21:57:48 +0000

    update 28/05/2018

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="./css/app.css" type="text/css" />
-        <title>Laravel - @yield('title')</title>
+        <title>@yield('title') - Laravel</title>
     </head>
     <body>
         <div class="wrap">
